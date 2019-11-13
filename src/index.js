943a38df39a88e02d0c52a0933d3009a0a063928
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2019-11-13 18:01:48 +0100

    npm broke becasue tried to install router-dom when code was running at the same time my subsystem terminal, had to reinstall everything... Now it works somehow...

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import NotFound from './NotFound'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
+
+const routing = (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </Router>
+)
+
+
+ReactDOM.render(routing, document.getElementById('root'));
