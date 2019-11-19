aa676c628a4af94f5f44976efb5c118b59ad6cdb
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2019-11-19 21:42:57 +0100

    Updates

---
+++ precache-manifest.6ccfe930766927012096a061745084d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad303072a5d3d6ccf50b6b0de9c37ad",
+    "url": "/dnd-gen/index.html"
+  },
+  {
+    "revision": "f9a758b23a3d73a25936",
+    "url": "/dnd-gen/static/css/main.1d9a953c.chunk.css"
+  },
+  {
+    "revision": "b9e8c52520fddd814595",
+    "url": "/dnd-gen/static/js/2.bb4d9ef8.chunk.js"
+  },
+  {
+    "revision": "f9a758b23a3d73a25936",
+    "url": "/dnd-gen/static/js/main.10a578cb.chunk.js"
+  },
+  {
+    "revision": "6f5df68fb34e832ffbf0",
+    "url": "/dnd-gen/static/js/runtime-main.0da2c2fd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0bc062820ad2282f125c169175716ad6.js"
+  "/dnd-gen/precache-manifest.6ccfe930766927012096a061745084d9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dnd-gen/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
