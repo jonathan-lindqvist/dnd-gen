1cb32efe1604838ff889895fb0178dde91cff8c0
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2019-11-19 21:38:43 +0100

    Updates

---
+++ precache-manifest.f61f82b1941e8400eb0c8c379d905d4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63cc0b368f55717e983520d055d03092",
+    "url": "/dnd-gen/index.html"
+  },
+  {
+    "revision": "d12b735cf33f18f1ebb5",
+    "url": "/dnd-gen/static/css/main.1d9a953c.chunk.css"
+  },
+  {
+    "revision": "b9e8c52520fddd814595",
+    "url": "/dnd-gen/static/js/2.bb4d9ef8.chunk.js"
+  },
+  {
+    "revision": "d12b735cf33f18f1ebb5",
+    "url": "/dnd-gen/static/js/main.103218be.chunk.js"
+  },
+  {
+    "revision": "6f5df68fb34e832ffbf0",
+    "url": "/dnd-gen/static/js/runtime-main.0da2c2fd.js"
+  }
+]);
